f72356da3c834bb2e1c60c66b12f0462cef85cfa
Nicole Miguel <nicolemonteiromiguel@gmail.com>
2022-09-19 00:38:00 -0300

    first commit

---
+++ Controllers/LojaController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Shopping.ViewModels;
+
+namespace Shopping.Controllers;
+
+public class LojaController : Controller
+{
+    //lista vazia para testar o if else:
+    private static List<LojaViewModel> lojas = new List<LojaViewModel>();
+
+   /* private static List<LojaViewModel> lojas = new List<LojaViewModel> {
+        new LojaViewModel(32, 3, "Tênis Brasil", "Aqui você encontra os tênis", "Loja", "tenis@email.com"),
+        new LojaViewModel(33, 3, "Lembranças Já", "Vem comprar sua lembrança","Kiosque", "lemb@email.com"),
+        new LojaViewModel(12, 1, "Sorvetinho Gelado", "Sorvetinho Gelado", "Kiosque", "sorvet@email.com")
+    }; */
+
+    public IActionResult Lista()
+    { 
+        return View(lojas);
+    }
+
+    public IActionResult Admin()
+    { 
+        
+        return View();
+    }
+
+    //detalhes
+    public IActionResult Detalhe(int id)
+    {
+        return View(lojas[id-1]);
+    }
+
+    public IActionResult Cadastrar([FromForm] int id, [FromForm] int piso, [FromForm] string nome, [FromForm] string descricao , [FromForm] string categoria, [FromForm] string email)
+    {
+        lojas.Add(new LojaViewModel(id, piso, nome, descricao, categoria, email));
+        return View();
+    }
+
+}
